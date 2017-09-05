3bfbb689ba32b55050ed3f31930a0e156b4f6bf4
Miron Ogrodowicz <miron.ogrodowicz@gmail.com>
2017-09-05 15:23:41 +0200

    added loop which waits for the application host to become available

--- supervisord/start-socat.sh
+++ supervisord/start-socat.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+while [ ! $(getent hosts application | awk '{ print $1 }') ]; do
+  true
+done
+
 socat TCP-LISTEN:8088,reuseaddr,fork TCP:$(getent hosts application | awk '{ print $1 }'):80 | sh
